d6f878bf9cc66f612ed9997a4e6a5ce713c18222
mweems <Thoughtworker@MW9591.local>
2013-08-21 11:15:51 -0400

    modified nokogiri check to find the correct elements

--- app/get_prices.rb
+++ app/get_prices.rb
@@ -7,8 +7,7 @@ class GetPrices
     @prices = get_prices(html)
   end
 
-  def lowest_price 
-    puts @prices 
+  def lowest_price  
     @prices.sort[0]
   end
 
@@ -16,7 +15,7 @@ class GetPrices
 
   def get_prices(html)
     doc = Nokogiri::HTML(html)
-    price_string = doc.css('span').text.strip
+    price_string = doc.css("span[class]").text.strip
     price_array = price_string.gsub("$","").split
     price_array.map { |price| price.to_f}
   end
