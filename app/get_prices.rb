b191afd31304f683cca91ac40132fd111da03d36
mweems <Thoughtworker@MW9591.local>
2013-08-21 09:40:44 -0400

    changed the method to scrape data from regex to nokgiri gem

--- app/get_prices.rb
+++ app/get_prices.rb
@@ -2,5 +2,27 @@ require '../helper/required.rb'
 
 class GetPrices
 
+  def initialize(html)
+    @prices = get_prices(html)
+  end
+
+  def lowest_price
+    @prices.sort[0]
+  end
+
+  private
+
+  def get_prices(html)
+    price_array = []
+    array = html.split
+    array.each do |word|
+      match = word.match(/([0-9][0-9]\.[0-9][0-9])/)
+      if match
+        price_array.push(match.captures[0].to_f)
+      end
+    end
+    price_array
+  end
+
 
 end
--- test/get_prices_test.rb
+++ test/get_prices_test.rb
@@ -1,12 +1,51 @@
+require '../helper/required.rb'
 require 'test/unit'
 require '../app/get_prices.rb'
 
 
-class GetPricesTest <Test::Unit::TestCase
 
-  def setup
-    @text = HTTParty.get('http://www.amazon.com/gp/offer-listing/0465026567/sr=/qid=/ref=olp_tab_all?ie=UTF8&colid=&coliid=&me=&qid=&seller=&sr=')
-    @user = GetPrices.new
+class GetPricesTest < Test::Unit::TestCase
+
+ 
+  #   @text = File.read('../helper/test_file.txt')
+  
+
+  def get_prices(html)
+    GetPrices.new(html)
+  end
+
+  def test_lowest_price_finds_a_single_price
+    html = <<-HTML
+    <html><body>
+    <span class='a-size-large a-color-price olpOfferPrice a-text-bold'>        $14.85    </span>
+    </body></html>
+    HTML
+    assert_equal(14.85, get_prices(html).lowest_price)
+  end
+
+  def test_lowest_price_returns_smallest_amount
+    html = <<-HTML
+    <html><body>
+    <span class='a-size-large a-color-price olpOfferPrice a-text-bold'>        $14.85    </span>
+    <span class='a-size-large a-color-price olpOfferPrice a-text-bold'>        $12.85    </span>
+    <span class='a-size-large a-color-price olpOfferPrice a-text-bold'>        $17.85    </span>    
+    </body></html>
+    HTML
+
+    assert_equal(12.85, get_prices(html).lowest_price)
+  end
+
+  def test_get_prices_ignores_numbers_that_arent_prices
+    html = <<-HTML
+    <html><body>
+    <span class='a-size-large a-color-price olpOfferPrice a-text-bold'>        $14.85    </span>
+    <span class='a-size-large a-color-price olpOfferPrice a-text-bold'>        $12.85    </span>
+    <span class='a-size-large a-color-price olpOfferPrice a-text-bold'>        $17.85    </span>
+    <span>        $11.85    </span>    
+    </body></html>
+    HTML
+
+    assert_equal(12.85, get_prices(html).lowest_price)    
   end
 
 end
